72d100dedfe735850add100db782b6cee708a896
Korben <korben.au@gmail.com>
2022-09-29 22:49:04 +1000

    initial

---
+++ kafka-basics/src/main/java/com/korben/demos/kafka/ProducerDemo.java
@@ -0,0 +1,14 @@
+package com.korben.demos.kafka;
+
+/**
+ * @ClassName ProducerDemo
+ * @Description TODO
+ * @Author gy
+ * @Date 29/9/2022 10:36 pm
+ **/
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
